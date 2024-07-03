cf15b7a24411efd415d6313ebb394b88825d22bf
Manabu TERADA <terada@cmscom.jp>
2024-07-03 11:28:03 +0900

    make qrcode app for basic

---
+++ app.py
@@ -0,0 +1,12 @@
+from io import BytesIO
+import streamlit as st
+import qrcode
+
+
+st.title("QRコード生成アプリ")
+text = st.text_input("QRコードに変換したい文字列を入力してください")
+if text and st.button("QRコードを生成する"):
+    with BytesIO() as output:
+        qrcode.make(text).save(output)
+        img = output.getvalue()
+    st.image(img, caption="QRコード")
